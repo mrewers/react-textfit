ec98471e9ea24716b7e77c38c57b5c950ef998d0
Marek Rewers <mrewers@users.noreply.github.com>
2020-04-25 19:33:41 -0400

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,23 @@
-'use strict';
-
-var webpack = require('webpack');
-
-var plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-];
-
-if (process.env.NODE_ENV === 'production') {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        })
-    );
-}
-
 module.exports = {
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: ['babel-loader'],
-            exclude: /node_modules/
-        }]
-    },
-    output: {
-        library: 'react-textfit',
-        libraryTarget: 'umd'
-    },
-    plugins: plugins,
-    resolve: {
-        extensions: ['', '.js']
-    }
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/u,
+        test: /\.ts(?<tsx>x?)$/u,
+        use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }],
+      },
+      {
+        exclude: /node_modules/u,
+        test: /\.js(?<jsx>x?)$/u,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    library: 'react-textfit',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
 };
